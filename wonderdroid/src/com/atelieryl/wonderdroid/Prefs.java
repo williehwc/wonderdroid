3af2b2f68ea60bcce2de772b0f4c927909af8335
williehwc <williehwc@gmail.com>
2018-04-22 12:00:50 -0400

    Reorganize preferences screen; add preference sub-screens

--- wonderdroid/src/com/atelieryl/wonderdroid/Prefs.java
+++ wonderdroid/src/com/atelieryl/wonderdroid/Prefs.java
@@ -1,17 +1,53 @@
 
 package com.atelieryl.wonderdroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.EditTextPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.widget.Toast;
 
 public class Prefs extends PreferenceActivity {
+	
+	PreferenceActivity activity = this;
+	boolean goBack = false;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.addPreferencesFromResource(R.layout.prefs);
-
-        EditTextPreference override = (EditTextPreference)findPreference("emu_overriderompath");
         getActionBar().setIcon(android.R.color.transparent);
+        findPreference("mapcontrols").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+            	goBack = true;
+            	activity.setPreferenceScreen(null);
+            	activity.addPreferencesFromResource(R.layout.mapcontrols);
+                return true;
+            }
+        });
+        findPreference("arrangeonscreencontrols").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+            	goBack = true;
+            	activity.setPreferenceScreen(null);
+            	activity.addPreferencesFromResource(R.layout.arrangeonscreencontrols);
+                return true;
+            }
+        });
     }
+    
+    @Override
+    public void onBackPressed() {
+    	if (goBack) {
+    		Intent intent = getIntent();
+    		intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+    		finish();
+    		startActivity(intent);
+    	} else {
+    		finish();
+    	}
+    }
+    
 }
